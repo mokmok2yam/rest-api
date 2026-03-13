3b0f7705cf30c7de94192ca20f240343ce6f622f
HanJeongmok <wjdahe1845@gmail.com>
2026-03-13 11:46:08 +0900

    46강 테스트케이스2개추가

--- src/main/java/com/back/global/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/back/global/exceptionHandler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.back.global.exceptionHandler;
 
 import com.back.global.rsData.RsData;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,7 +20,7 @@ public class GlobalExceptionHandler {
     public RsData<Void> handleException(){
         return new RsData<Void>(
                 "존재하지 않는 데이터입니다.",
-                "400-1"
+                "404-1"
         );
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -37,6 +38,15 @@ public class GlobalExceptionHandler {
         return new RsData<Void>(
                 message,
                 "400-1"
+        );
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseBody
+    public RsData<Void> handleException(HttpMessageNotReadableException e) {
+        return new RsData<Void>(
+                "잘못된 형식의 요청 데이터입니다.",
+                "400-2"
         );
     }
 }
--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -84,7 +84,7 @@ public class ApiV1PostControllerTest {
     }
 
     @Test
-    @DisplayName("글 단건 조회, 실패 존해하지않는글")
+    @DisplayName("글 단건 조회 - 실패(존재하지 않는 글)")
     void t3() throws Exception {
         int targetId = Integer.MAX_VALUE;
 
@@ -161,11 +161,63 @@ public class ApiV1PostControllerTest {
 
 
     }
+    @Test
+    @DisplayName("글 작성, 내용이 입력되지 않은 경우")
+    void t6() throws Exception {
+        String title = "제목입니다.";
+        String content = "";
 
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "title": "%s",
+                                            "content": "%s"
+                                        }
+                                        """.formatted(title, content))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("write"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.resultCode").value("400-1"))
+                .andExpect(jsonPath("$.msg").value("content-NotBlank-02-content-내용은 필수입니다.\ncontent-Size-04-content-내용은 2자 이상 100자 이하로 입력해주세요."));
+    }
+
+    @Test
+    @DisplayName("글 작성, JSON 양식이 잘못된 경우")
+    void t7() throws Exception {
+        String title = "제목입니다.";
+        String content = "내용입니다";
+
+        ResultActions resultActions = mvc
+                .perform(
+                        post("/api/v1/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {
+                                            "title": "%s"
+                                            "content": "%s"
+                                        
+                                        """.formatted(title, content))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("write"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.resultCode").value("400-2"))
+                .andExpect(jsonPath("$.msg").value("잘못된 형식의 요청 데이터입니다."));
+    }
 
     @Test
     @DisplayName("글 수정")
-    void t6() throws Exception {
+    void t8() throws Exception {
         Integer targetId = 1;
         String title = "제목 수정";
         String content = "내용 수정";
@@ -199,7 +251,7 @@ public class ApiV1PostControllerTest {
     }
     @Test
     @DisplayName("글 삭제")
-    void t7() throws Exception {
+    void t9() throws Exception {
         int targetId = 1;
 
         ResultActions resultActions = mvc
